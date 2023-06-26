871d14a70ef26c12c9e0618d014126dcf97aaa79
Malik A. Aziz Lubis <malik.lubis@gmail.com>
2023-06-26 23:29:46 +0700

    add third view

--- TabBar - Storyboard/SecondViewController.swift
+++ TabBar - Storyboard/SecondViewController.swift
@@ -12,18 +12,12 @@ class SecondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        title = "Second Bar Title"
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func btnNextPage(_ sender: Any) {
+        let storyboard = self.storyboard?.instantiateViewController(withIdentifier: "ThirdViewController") as! ThirdViewController
+        self.navigationController?.pushViewController(storyboard, animated: true)
     }
-    */
 
 }
