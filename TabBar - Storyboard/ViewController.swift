8577cecb2c2921963e7a42ab4fdb81f993d43b40
Malik A. Aziz Lubis <malik.lubis@gmail.com>
2023-06-26 22:36:54 +0700

    add secondviewviewcontroller

--- TabBar - Storyboard/ViewController.swift
+++ TabBar - Storyboard/ViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
 
+    @IBOutlet var searchBar: UISearchBar!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Bar Item"
+        self.navigationItem.titleView = searchBar
+        searchBar.placeholder = "Search ..."
+        searchBar.delegate = self
     }
 
-
+    @IBOutlet weak var txtSearch: UILabel!
+    @IBAction func btnSearch(_ sender: Any) {
+        
+        if (searchBar.text == "") {
+            txtSearch.text = "Kosong"
+        } else {
+            txtSearch.text = searchBar.text
+        }
+        
+        searchBar.resignFirstResponder() // Menutup keyboard
+    }
+    
 }
-
